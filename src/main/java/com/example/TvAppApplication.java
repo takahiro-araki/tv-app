989d0a59f4fc4cc008cfd608535d7790a8e09506
takahiroaraki <s01110002e1117.gmail.com>
2019-11-04 14:54:00 +0900

    ファーストコミット

---
+++ src/main/java/com/example/TvAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvAppApplication.class, args);
+	}
+
+}
